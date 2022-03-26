db2ebafa3f3102e30e2908e72655749805aa97b6
Avnish <shyam_gade2001@yahoo.co.in>
2022-03-26 10:45:28 +0530

    This is my First Commit....

---
+++ src/practice/DemoGit1.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class DemoGit1 {
+
+	public static void main(String[] args) {
+		String Hungry="Yes";
+		if(Hungry=="Yes")
+			System.out.println("Eat Something");
+		else
+			System.out.println("do work");
+
+	}
+
+}
